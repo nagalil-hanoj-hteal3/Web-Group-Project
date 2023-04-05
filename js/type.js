07bb380ff4e1a5d9430580c183a75363e03879ed
Your Name <kyrstn.hall@gmail.com>
2023-04-05 16:14:32 -0700

    type js and accessories pics

--- js/type.js
+++ js/type.js
@@ -1,11 +1,12 @@
 var typed = new Typed(".auto-type", {
-    strings: ["Ever wondered who we are?", 
+    /*strings: ["Ever wondered who we are?", 
             "Meet the Team of FitRat here!", 
             "Cool Edit Eh?", 
             "Anyways...", 
             "Back to the Original :)",
-            "<u>FitRat Crew!</u>"],
-    typeSpeed: 30,
-    backSpeed: 30,
-    loop: false
+            "<u>FitRat Crew!</u>"],*/
+    strings: ["racecar"],
+    typeSpeed: 0.1,
+    backSpeed: 0.1,
+    loop: true
 })
