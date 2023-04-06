3740095263c7a4bb7041fe76d9e5b5dae8f71d95
Your Name <kyrstn.hall@gmail.com>
2023-04-05 21:01:38 -0700

    added $ and women bottom

--- js/type.js
+++ js/type.js
@@ -1,11 +1,11 @@
 var typed = new Typed(".auto-type", {
-    /*strings: ["Ever wondered who we are?", 
+    strings: ["Ever wondered who we are?", 
             "Meet the Team of FitRat here!", 
             "Cool Edit Eh?", 
             "Anyways...", 
             "Back to the Original :)",
-            "<u>FitRat Crew!</u>"],*/
-    strings: ["racecar"],
+            "<u>FitRat Crew!</u>"],
+    //strings: ["racecar"],
     typeSpeed: 0.1,
     backSpeed: 0.1,
     loop: true
