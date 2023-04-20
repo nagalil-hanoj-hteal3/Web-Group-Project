9c66a2f3cb30f62e1ff066f5793e327416b9cabb
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2023-04-19 18:26:35 -0700

    changes

---
+++ js/service.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.collapse-title').click(function(){
+        $('.collapse-details').toggle();
+    });
+
+});
--- js/type.js
+++ js/type.js
@@ -1,12 +1,7 @@
 var typed = new Typed(".auto-type", {
-    strings: ["Ever wondered who we are?", 
-            "Meet the Team of FitRat here!", 
-            "Cool Edit Eh?", 
-            "Anyways...", 
-            "Back to the Original :)",
-            "<u>FitRat Crew!</u>"],
+    strings: ["Welcome to FitRat, where fitness meets fashion! Our journey started with a simple idea to create high-quality fitness apparel for individuals to start an active and healthy lifestyle. Inspired by our love for fitness and fashion, FitRat was founded by a group of fitness enthusiasts who believe that looking and feeling good go hand-in-hand. We understand that workout gear should not only perform well, but also make you feel confident and stylish during your fitness journey. Driven by our passion for fitness and fashion, we have curated a premium collection of fitness shirts, pants, and accessories that are designed to meet the needs of modern-day fitness enthusiasts. Our products are made from top-quality materials that offer comfort, durability, and performance, so you can push your limits in style. At FitRat, we are more than just a brand - we are a community of fitness enthusiasts who support and motivate each other to strive for greatness. We believe that fitness is a journey, not a destination, and we are committed to providing our customers with the best fitness apparel to help them achieve their fitness goals. Whether you are hitting the gym, going for a run, or practicing yoga, FitRat has the perfect gear for you. Join us on this fitness adventure and elevate your fitness wardrobe with FitRat's premium collection. Welcome to the FitRat family, where fitness meets fashion and style meets performance! Let's embark on this fitness journey together and make every workout count!"],
     //strings: ["racecar"],
-    typeSpeed: 0.1,
-    backSpeed: 0.1,
-    loop: true
+    typeSpeed: 30,
+    //backSpeed: 0.1,
+    //loop: true
 })
