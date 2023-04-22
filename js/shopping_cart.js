2db9c902238cd56d79ac6143720bfe9ebf6c1ccd
myd28 <my.dang2028@gmail.com>
2023-04-22 15:53:01 -0700

    Co-authored-by: nagalil-honaj-hteal3 <nagalil-honaj-hteal3@users.noreply.github.com>

---
+++ js/shopping_cart.js
@@ -0,0 +1,17 @@
+var idString= localStorage.getItem("shopping_cart_item"); 
+var id= JSON.parse(idString); 
+var item_id= id.id; 
+
+
+//document.getElementById("item_in_cart").innerHTML=`<img src="${item_img}">`
+
+//document.getElementById("shopping-cart").innerHTML=``
+
+
+
+/*const women_sweatpant_items_String=JSON.stringify(women_sweatpant_items[i]);
+localStorage.setItem("shopping_cart_items",women_sweatpant_items_String );
+const stored_dataset= localStorage.getItem("shopping_cart_items");
+const read_dataset= JSON.parse(stored_dataset); 
+console.log(read_dataset); 
+event.preventDefault(); */
--- js/women_bottom.js
+++ js/women_bottom.js
@@ -36,12 +36,15 @@ let women_sweatpant_items= [
 
 ]
 
+//convert dataset into JSON string
+const women_sweatpant_items_String=JSON.stringify(women_sweatpant_items);
+
 let women_sweatpant_item_container= document.querySelector('.women_sweatpant_items')
   
     for (let item of women_sweatpant_items){
         women_sweatpant_item_container.innerHTML+=`<td><img onmouseout="mouseOut_sweatpant(event)" onmouseover="mouseOver_sweatpant(event)" data-itemid="${item.id}" src="${item.img}"  width="150px" height="210px"/><br>
         <label>${item.name}</label><br><label>$${item.price}</label>
-        <button>Add to Cart</button>
+        <button>Add To Cart</button>
         </td>`
     }
 
@@ -51,5 +54,33 @@ function mouseOut_sweatpant(event){
 function mouseOver_sweatpant(event){
     event.target.src=women_sweatpant_items.find(obj=>obj.id==event.target.dataset.itemid).img_hover
 }
+
+//empty array to contain items from women section 
+let items_array=[]; 
+let counter = 0;
+  
+
+const cart_buttons=document.querySelectorAll("button"); 
+
+for(let i=0; i<cart_buttons.length; i++){
+    console.log("here// testing")
+    cart_buttons[i].addEventListener("click", function(event){
+        //console.log(women_sweatpant_items[i].id); 
+        //alert("Item has been added to cart!!"); 
+        //items_array.push(women_sweatpant_items[i]);
+        //console.log(items_array);
+
+        //convert dataset into JSON string
+        const women_sweatpant_items_String=JSON.stringify(women_sweatpant_items[i]);
+        localStorage.setItem("shopping_cart_items",women_sweatpant_items_String );
+        const stored_dataset= localStorage.getItem("shopping_cart_items");
+        const read_dataset= JSON.parse(stored_dataset); 
+        console.log(read_dataset); 
+        event.preventDefault(); 
+    })
+    
+}
+
+
 
 
