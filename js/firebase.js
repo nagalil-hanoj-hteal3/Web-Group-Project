f0794f725e21c76bd3d38d7b9cacadc26e58a9fe
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2023-05-25 23:35:03 -0700

    login + register + logout done, edit the styles

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -96,6 +96,9 @@ onAuthStateChanged(auth, (user) => {
         // ...
     }
 });
+
+
+const logout = document.getElementById('logoutButton');
 
 logout.addEventListener('click', (e) =>{
     signOut(auth).then(() => {
